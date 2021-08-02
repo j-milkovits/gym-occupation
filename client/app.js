62660eb87e9c1d97c3d6b079fb6463ffdb8504e8
j-milkovits <j.milkovits.t@posteo.net>
2021-08-02 20:05:25 +0200

    initial commit for client side stuff

---
+++ client/app.js
@@ -0,0 +1,27 @@
+
+// Code for graph
+// var ctx = document.getElementById('chartCanvas').getContext('2d');
+// var chartCanvas = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: [1,2,3,4,5],
+//         datasets: [{
+//             label: 'Occupation in Fitness First',
+//             data: [1,2,3,4,5],
+//             backgroundColor: [
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1,
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
